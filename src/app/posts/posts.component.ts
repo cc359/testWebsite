ba9f23e07d28c21c7e8b55ca56aead56bb9f28e9
Chris Cooper <chrisjc1202@gmail.com>
2018-12-30 03:06:20 +0000

    testWebsite

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  constructor(private data: DataService) { }
+  posts$: Object;
+
+  ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.posts$ = data
+    )
+  };
+
+}
---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'
+
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.scss']
+})
+export class SideBarComponent implements OnInit {
+
+  currentUrl: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  }
+
+  ngOnInit() {}
+
+}
